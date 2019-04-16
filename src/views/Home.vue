3f0a21886084dfb51ea5668e7af64ba0bae5bbcf
buqianqian <732216761@qq.com>
2019-04-16 17:20:47 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,24 +15,21 @@
 <script>
 import Banner from '@/components/Banner.vue'
 import Tab from '@/components/Tab.vue'
-import BScroll from 'better-scroll'
+// import BScroll from 'better-scroll'
 // import Main from '@/components/Main.vue'
 
 export default {
   name: 'home',
   components: {
     Banner,
-    // HelloWorld,
     Tab
-    // Main
   },
   data () {
     return {
       wrapperHeight: 0,
       isBottom: false,
       isShow: false,
-      allLoaded: false,
-      height: 1000
+      allLoaded: false
     }
   },
   mounted () {
@@ -42,7 +39,6 @@ export default {
         this.$refs.wrapper.getBoundingClientRect().top
     }, 200)
     this.$refs.aa.addEventListener('scroll', () => {
-      this.height = document.getElementsByClassName('mint-loadmore')[0].clientHeight
       // console.log(this.$refs.aa.scrollTop)
       if (this.$refs.aa.scrollTop >= 150) {
         this.isShow = true
@@ -50,10 +46,10 @@ export default {
         this.isShow = false
       }
     }, false)
-    console.log(document.getElementsByClassName('mint-loadmore')[0].clientHeight)
-    this.$nextTick(() => {
-      this.scroll = new BScroll(this.$refs.wrapper, {})
-    })
+    // console.log(document.getElementsByClassName('mint-loadmore')[0].clientHeight)
+    // this.$nextTick(() => {
+    //   this.scroll = new BScroll(this.$refs.wrapper, {})
+    // })
   },
   methods: {
     loadTop () {
